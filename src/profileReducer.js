64cd833920883dba02779c96fea6984176e54358
Kamal-Aepuru <kamalaepuru@gmail.com>
2025-01-08 12:21:27 +0530

    feat:remove profile updated

--- src/profileReducer.js
+++ src/profileReducer.js
@@ -8,25 +8,31 @@ const initialState = {
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_PROFILE:
-      return { ...state, profileList: [...state.profileList, action.payload] };
+      return {
+        ...state,
+        profileList: [...state.profileList, action.payload],
+      };
+
     case REMOVE_PROFILE: {
-      const profileToBeRemovedIndex = state.profileList.findIndex(
-        (profile) => profile.id === action.payload
-      );
-      const updatedProfileList = [...state.profileList];
-      if (profileToBeRemovedIndex >=0) {
-        updatedProfileList.splice(profileToBeRemovedIndex, 1);
-      }
-      return { ...state, profileList: updatedProfileList };
+      return {
+        ...state,
+        profileList: state.profileList.filter(
+          (profile) => profile.id !== action.payload
+        ),
+      };
     }
+
     case CALCULATE_AVERAGE_AGE: {
-      if (!state.profileList || state.profileList.length === 0) {
+      if (state.profileList.length === 0) {
         return { ...state, average: 0 };
       }
-      const averageAge = parseFloat(
-        state.profileList.reduce((acc, curr) => acc + curr.age, 0) /
-          state.profileList.length
-      ).toFixed(2);
+      const totalAge = state.profileList.reduce(
+        (acc, curr) => acc + curr.age,
+        0
+      );
+      const averageAge = Number(
+        (totalAge / state.profileList.length).toFixed(2)
+      );
 
       return { ...state, average: averageAge };
     }
